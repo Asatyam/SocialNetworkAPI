30e012ce9b3d8935ef97aa6e6408916aa6493348
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-26 21:37:58 +0530

    Define the user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  likes: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  first_name: { type: String },
+  last_name: { type: String },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  facebook_id: { type: String },
+  image_url: { type: String },
+});
+
+module.exports = mongoose.model('User', UserSchema);
