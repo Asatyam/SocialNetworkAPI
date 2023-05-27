0e97748cbfe0257ebe8946ad97e82a2b23622b31
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-27 12:34:56 +0530

    Export passport configs from its file

--- helpers/passport.js
+++ helpers/passport.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable-next-line consistent-return */
 const bcryptjs = require('bcryptjs');
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JWTStrategy = require('passport-jwt').Strategy;
 const FBStrategy = require('passport-facebook').Strategy;
@@ -12,83 +11,88 @@ const User = require('../models/User');
 
 require('dotenv').config();
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
+module.exports = function setUpPassport(passport) {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser(async (id, done) => {
     try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return done(null, false, { messsage: 'User not found' });
-      }
-      bcryptjs.compare(password, user.password, (err, res) => {
-        if (res) {
-          return done(null, user);
-        }
-        return done(null, false, { message: 'Incorrect Password' });
-      });
+      const user = await User.findById(id);
+      done(null, user);
     } catch (err) {
-      return done(err, false);
+      done(err);
     }
-  })
-);
-
-passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.SECRET,
-    },
-    (jwtPayload, done) => done(null, jwtPayload)
-  )
-);
-
-passport.use(
-  new FBStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: 'http://localhost:4000/login/facebook/callback',
-      profileFields: [
-        'id',
-        'emails',
-        'first_name',
-        'last_name',
-        'picture.type(medium)',
-      ],
-    },
-    (accessToken, refreshToken, profile, done) => {
-      const picture = `https://graph.facebook.com/${profile.id}/picture?width=200&height=200&acess_token=${accessToken}`;
-
-      User.findOne({ facebook_id: profile.id }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+  });
+  passport.use(
+    'local-login',
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
         if (!user) {
-          user = new User({
-            facebook_id: profile.id,
-            first_name: profile.first_name,
-            last_name: profile.last_name,
-            email: profile.emails[0].value,
-            image_url: picture,
-          });
-          user.save((err) => {
-            if (err) console.log(err);
-            return done(err, user);
-          });
-        } else {
-          return done(err, user);
+          return done(null, false, { messsage: 'User not found' });
         }
-      });
-    }
-  )
-);
+        bcryptjs.compare(password, user.password, (err, res) => {
+          if (res) {
+            return done(null, user);
+          }
+          return done(null, false, { message: 'Incorrect Password' });
+        });
+      } catch (err) {
+        return done(err, false);
+      }
+    })
+  );
+
+  passport.use(
+    'jwt',
+    new JWTStrategy(
+      {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.SECRET,
+      },
+      (jwtPayload, done) => done(null, jwtPayload)
+    )
+  );
+
+  passport.use(
+    'facebook-login',
+    new FBStrategy(
+      {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: 'http://localhost:4000/login/facebook/callback',
+        profileFields: [
+          'id',
+          'emails',
+          'first_name',
+          'last_name',
+          'picture.type(medium)',
+        ],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const picture = `https://graph.facebook.com/${profile.id}/picture?width=200&height=200&acess_token=${accessToken}`;
+
+        User.findOne({ facebook_id: profile.id }, (err, user) => {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            user = new User({
+              facebook_id: profile.id,
+              first_name: profile.first_name,
+              last_name: profile.last_name,
+              email: profile.emails[0].value,
+              image_url: picture,
+            });
+            user.save((err) => {
+              if (err) console.log(err);
+              return done(err, user);
+            });
+          } else {
+            return done(err, user);
+          }
+        });
+      }
+    )
+  );
+};
